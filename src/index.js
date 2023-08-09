0adc8872d8b61bd80117e7ac2ee58f1cb87ae1d7
MariaBetancur <mabo6241@gmail.com>
2023-08-09 17:53:41 -0500

    v1.2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { GlobalStyle } from './index.styled';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={'/goit-react-hw-05-moviess/'}>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
